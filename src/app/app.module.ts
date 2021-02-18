538fae2117e41d64c675e85caf23acfa1f4f8dad
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-02-18 13:14:00 -0500

    added component and pages and moved html and css for header to header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { CheckoutComponent } from './Pages/checkout/checkout.component';
+import { LoginComponent } from './Pages/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent,
+      HeaderComponent
   ],
   imports: [
     BrowserModule,
