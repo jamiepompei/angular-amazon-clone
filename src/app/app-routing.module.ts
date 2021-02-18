60249f7f8619158ad08808057706d5108b392b28
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-02-18 15:25:16 -0500

    aded routing for checkout and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './Pages/checkout/checkout.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppComponent
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
   }
 ];
 
