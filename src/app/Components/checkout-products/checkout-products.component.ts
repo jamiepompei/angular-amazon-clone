fc57894e6d3459713307ad9d53aef99c095571f0
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-02-19 15:53:39 -0500

    added HTML for checkout component and css for checkout component

--- src/app/Components/checkout-products/checkout-products.component.ts
+++ src/app/Components/checkout-products/checkout-products.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ShoppingCartService } from 'src/app/Services/shopping-cart.service';
 
 @Component({
   selector: 'app-checkout-products',
@@ -7,10 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CheckoutProductsComponent implements OnInit {
 @Input() checkout_products: any[];
-  constructor() { }
+@Output() deleteEvent: EventEmitter<any> = new EventEmitter();
+
+  constructor(public shopping_cart_service: ShoppingCartService) { }
 
   ngOnInit(): void {
     console.log('products ', this.checkout_products);
+  }
+  removeItem(p: any){
+    this.shopping_cart_service.removeItem(p);
+    this.deleteEvent.emit(p);
   }
 
 }
