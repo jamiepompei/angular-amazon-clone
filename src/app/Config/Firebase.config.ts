6af86518011e514fa1687bb0b3fd91adf35b63b7
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-02-25 15:05:57 -0500

    added firebase configuration, added bones for form login on login page, added an auth service

---
+++ src/app/Config/Firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB2jIXw0m6ilo_cYJS6mZAouTG35X6IJi8",
+    authDomain: "cloneangular-7d98a.firebaseapp.com",
+    projectId: "cloneangular-7d98a",
+    storageBucket: "cloneangular-7d98a.appspot.com",
+    messagingSenderId: "770957221",
+    appId: "1:770957221:web:6a6de7a673d842820c924c",
+    measurementId: "G-TSF61Y6YVK"
+};
